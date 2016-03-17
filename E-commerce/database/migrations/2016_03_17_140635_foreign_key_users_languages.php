36d3af0bd9929dc9bc5486dde8350dab2551ddac
apichonnat <alain.pichonnat@cpnv.ch>
2016-03-17 15:15:35 +0100

    premier construction de liaisin (foreign key)

---
+++ E-commerce/database/migrations/2016_03_17_140635_foreign_key_users_languages.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ForeignKeyUsersLanguages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->foreign('language_id')->references('id')->on('languages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->dropForeign(['language_id']);
+        });
+    }
+}
